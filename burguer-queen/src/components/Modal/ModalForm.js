43f9362c91436cb35040e2865f523f2f053aac0a
LaubetBeltran <laubetpop@gmail.com>
2021-05-07 19:22:10 -0500

    Form para agregar productos min 1

--- burguer-queen/src/components/Modal/ModalForm.js
+++ burguer-queen/src/components/Modal/ModalForm.js
@@ -21,26 +21,28 @@ function ModalForm(props) {
     })
   }
 
+	const enviarDatos = (event) =>{
+		event.preventDefault();
+		props.data.handleAddProducts(addProduct);
+		dialog.close(addProduct);
+	}
+
   return (
     <div className="modal">
+			<form className="row" onSubmit={enviarDatos}>
       {/* {console.log("props", props.data.product.id)} */}
       <h1>{props.data.product.name}</h1>
       <img className="img" src={props.data.product.image} alt="Img Logo" />
       <div className='label'>
       <h3 className="labelValue">Cantidad</h3>
-      <input type="number" className="inputForm" id="amount" name="amount" onChange={addPoducts} pattern="[0,9]{0,13}"/>
+      <input type="number" className="inputForm" id="amount" name="amount" onChange={addPoducts} pattern="[0,9]{0,13}" required="required" min={1}/>
       </div>
       <h3 className="labelValue">Especificaciones</h3>
       <textarea className="specs" id="specs" name="specs" onChange={addPoducts}></textarea>
-      <button className="buttonAdd"
-        onClick={() => {
-          props.data.handleAddProducts(addProduct)
-          // Сlose the dialog and return the value
-          dialog.close(addProduct);
-        }}
-      >
+      <button className="buttonAdd">
         Añadir Producto
       </button>
+			</form>
     </div>
   );
 }
