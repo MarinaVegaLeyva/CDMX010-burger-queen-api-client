198ca770e84703c96e16b8fffd72ba88cc6b5b1e
MarinaVegaLeyva <marina.vega.condor@gmail.com>
2021-05-11 12:05:56 -0500

    intentando subir datos a order

--- burguer-queen/src/App.js
+++ burguer-queen/src/App.js
@@ -1,93 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Menu from "./pages/Menu";
-import Login from './pages/Login/Login';
-import WaitersMain from './pages/WaitersMain/WaitersMain';
-
+import Login from "./pages/Login/Login";
+import WaitersMain from "./pages/WaitersMain/WaitersMain";
 
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	// Link,
-	// NavLink,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link,
+  // NavLink,
 } from "react-router-dom";
-import OrderCardsContainer from './components/OrderCardsWaiters/MainContainer/OrderCardsContainer';
-
+import OrderCardsContainer from "./components/OrderCardsWaiters/MainContainer/OrderCardsContainer";
 
 function App() {
-	const [order, setOrder] = useState({
-		id: "",
-		userId: "",
-		client: "",
-		products: [],
-		status: "pending",
-		dateEntry: "10-05-2021",
-		dateProcessed: "??",
-		total: 0
-	})
+  const [order, setOrder] = useState({
+    id: "",
+    userId: "",
+    client: "",
+    products: [],
+    status: "pending",
+    dateEntry: "",
+    dateProcessed: "??",
+    total: 0,
+  });
 
-	// function App() {
-	// 	const [order, setOrder] = useState({
-	// 		nombreCliente: "",
-	// 		status: "pendiente",
-	// 		items: [],
-	// 		total: 0
-	// 	})
+  const handleAddProducts = (item) => {
+    setOrder({ ...order, products: [...order.products, item] });
+  };
 
-		const handleAddProducts = (item) => {
-			setOrder({ ...order, products: [...order.products, item] })
-		}
+  const handleAddInfoOrder = ( propiedad, item ) => {
+    setOrder({ ...order, [propiedad]:item });
+  };
+  const handleDeleteProducts = (arrItems) => {
+    setOrder({ ...order, products: arrItems });
+  };
 
-		const handleDeleteProducts = (arrItems) => {
-			setOrder({ ...order, products: arrItems })
-		}
+  return (
+    <Router>
+      <Switch>
+        <Route path="/waitersmain">
+          <WaitersMain />
+        </Route>
+        <Route path="/menu">
+          <div>
+            <Menu
+              handleAddProducts={handleAddProducts}
+              order={order}
+              handleDeleteProducts={handleDeleteProducts}
+              handleAddInfoOrder={handleAddInfoOrder}
+            />
+          </div>
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
 
-
-		return (
-			<Router>
-				<Switch>
-					<Route path="/waitersmain">
-						<WaitersMain />
-					</Route>
-					<Route path="/menu">
-						<div>
-							<Menu
-								handleAddProducts={handleAddProducts}
-								order={order}
-								handleDeleteProducts={handleDeleteProducts}
-							/>
-						</div>
-					</Route>
-					<Route path="/">
-						<Login />
-					</Route>
-				</Switch>
-			</Router>
-		);
-	}
-
-	export default App;
-
-
-
-// import './style/App.css';
-// import React, { useState } from "react";
-// import Modal from 'react-modal';
-
-
-// function App() {
-// 	const [modalIsOpen, setModalIsOpen] = useState(false)
-//   return (
-//     <div className="App">
-// 			<button onClick={() => setModalIsOpen(true)}>Modal</button>
-// 				<Modal isOpen={modalIsOpen}>
-// 					<div>holaaaa</div>
-// 					<div>
-// 						<button onClick={() => setModalIsOpen(false)}>close</button>
-// 					</div>
-// 				</Modal>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
