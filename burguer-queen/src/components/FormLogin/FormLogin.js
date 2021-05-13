959b4266a2d3705c2b8dcb468221953181476542
LaubetBeltran <laubetpop@gmail.com>
2021-05-12 19:13:27 -0500

    Boón de Login direcciona a WaiterMain

--- burguer-queen/src/components/FormLogin/FormLogin.js
+++ burguer-queen/src/components/FormLogin/FormLogin.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import './FormLogin.css';
 const FormLogin = () => {
+	const history = useHistory();
+	const handleLogin = () =>{
+		history.push("/waitersmain")
+	}
 	return(
 		<form id="loginForm" className="LoginForm">
 			<input type="email" id="inputEmail" className="loginInput" placeholder="Correo electrónico"></input>
 			<input type="password" id="inputPassword" className="loginInput" placeholder="Contraseña"></input>
-			<button id="loginButton" className="sendButton">Ingresar</button>
+			<button id="loginButton" className="sendButton" onClick={handleLogin}>Ingresar</button>
 		</form>
 	)
 };
